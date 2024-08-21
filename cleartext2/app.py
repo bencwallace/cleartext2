0910d27ea315ffe0008df5b52b0b4db08e38e7fc
Ben Wallace <bencwallace@gmail.com>
2024-08-21 15:11:29 +0200

    add simple evaluation script using LexMTurk

--- cleartext2/app.py
+++ cleartext2/app.py
@@ -14,6 +14,6 @@ class SelectionInContext(BaseModel):
 
 @app.post("/")
 async def root(context: SelectionInContext):
-    pipeline = predict_weighted.Pipeline()
+    pipeline = predict_weighted.Pipeline(top_k=5, likelihood_weight=0, frequency_weight=1)
     top_tok, _ = pipeline.predict(context.before, context.selection, context.after)
     return top_tok
